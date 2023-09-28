3eb95828e2f60f11d396a294dadc454f91388707
Alejandro Ronconi <ronconialejandro@gmail.com>
2023-09-28 00:59:43 -0300

    Configuracion de tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,15 @@
 export default {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      md: "768px",
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
