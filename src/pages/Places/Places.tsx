305b73b83702ce0f82a14e84912a3465643e23ec
Alejandro Ronconi <ronconialejandro@gmail.com>
2023-10-15 02:05:24 -0300

    Actualizo spinner para seccion de Lugares

--- src/pages/Places/Places.tsx
+++ src/pages/Places/Places.tsx
@@ -4,9 +4,15 @@ import { Layout } from "../../components/layout";
 import { onGetGeoloc, deleteGeoloc } from "../../../firebase";
 import { IGeoData } from "../../utils/interfaces";
 import { ListLocation } from "../../components/ListLocation";
+import { useGeoStore } from "../../store/store";
+import Spinner from "../../components/Spinner/Spinner";
+import Modal from "../../components/Modal/Modal";
 
 export const Places = () => {
   const [locations, setLocations] = useState<unknown[] | IGeoData[] | null>([]);
+  const { setShowLoadingSpiner, loadingSpinner } = useGeoStore();
+  const [showModalSucces, setShowModalSucces] = useState(false);
+  const [showModalError, setShowModalError] = useState(false);
 
   useEffect(() => {
     async function fetchLocations() {
@@ -15,7 +21,12 @@ export const Places = () => {
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
         onGetGeoloc((querySnapshot: { data: () => any }[]) => {
           // eslint-disable-next-line @typescript-eslint/no-explicit-any
-          const locations: any[] | ((prevState: unknown[] | IGeoData[] | null) => unknown[] | IGeoData[] | null) | null = [];
+          const locations:
+            | any[]
+            | ((
+                prevState: unknown[] | IGeoData[] | null
+              ) => unknown[] | IGeoData[] | null)
+            | null = [];
           // eslint-disable-next-line @typescript-eslint/no-explicit-any
           querySnapshot.forEach((doc: { data: () => any }) => {
             const geoloc = doc.data();
@@ -32,15 +43,58 @@ export const Places = () => {
     fetchLocations();
   }, []);
 
+  const deleteLocation = (id: string) => {
+    setShowLoadingSpiner(true);
+    setTimeout(() => {
+      try {
+        deleteGeoloc(id);
+        setShowModalSucces(true)
+      } catch (error) {
+        console.error("No se pudo eliminar la localización");
+        setShowModalError(true)
+      } finally {
+        setShowLoadingSpiner(false);
+      }
+    }, 1000);
+  };
+
   return (
     <Layout title="places" subtitle="Mis lugares">
       <div className="flex absolute top-24 md:top-40 w-full justify-center items-start">
         <ListLocation
           data={locations}
-          deleteLocation={deleteGeoloc}
+          deleteLocation={deleteLocation}
           modifyLocation={() => console.log()}
         />
       </div>
+      {loadingSpinner && <Spinner data={"Loadings"} color="delete" />}
+      {showModalSucces && (
+        <Modal
+          onPrimaryAction={() => setShowModalSucces(false)}
+          data={{
+            title: "Perfecto",
+            textButton: "Entendido",
+            descripton: "Borraste la localización con éxito",
+            icon: undefined,
+            type: "success",
+          }}
+        />
+      )}
+      {showModalError && (
+        <Modal
+          onPrimaryAction={() => {
+            setShowModalError(false);
+          }}
+          data={{
+            title: "Algo salió mal",
+            textButton: "Entendido",
+            descripton:
+              "No se pudo borrar la localización. Volvé a intentarlo más tarde",
+            icon: undefined,
+            type: "error",
+          }}
+        />
+      )}
     </Layout>
   );
 };
--- src/utils/interfaces.ts
+++ src/utils/interfaces.ts
@@ -1,16 +1,22 @@
- export type IPathId = 'home' | 'about' | 'places'
+export type IPathId = "home" | "about" | "places";
 
 export interface IItems {
-    id: IPathId
-    path: string
-}[]
-
+  id: IPathId;
+  path: string;
+}
+[];
 
 export type IModalType = "success" | "error" | "alert";
 
 export interface IGeoData {
-    name: string
-    description: string
-    lat: number
-    lng: number
+  name: string;
+  description: string;
+  lat: number;
+  lng: number;
 }
+
+export interface IColorSpinner {
+  color: 'save' | 'delete' | 'edit' 
+}
+
+export type ILoaderColor = "save" | "delete" | "edit";
