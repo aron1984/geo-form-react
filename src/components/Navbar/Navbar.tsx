c3c4314c6fb2975c36ea50e8c3228216487b59c5
Alejandro Ronconi <ronconialejandro@gmail.com>
2023-11-01 18:08:13 -0300

    Agreo user a navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -7,16 +7,21 @@ interface Props {
   items: IItems[];
 }
 
+const user = {
+  name: "Ale",
+  profile: "admin",
+};
+
 export const Navbar: FC<Props> = ({ items }) => {
   const location = useLocation();
 
   return (
     <div className="bg-slate-800 h-10 md:h-20 shadow-lg w-full z-50 fixed">
-      <div className="flex h-full items-center justify-between">
+      <div className="flex h-full items-center justify-end">
         <img
           src="/img/g-logo_myv.svg"
           alt="Geo Form"
-          className="px-2 h-6 md:pl-5 md:h-8"
+          className="px-2 h-6 mr-auto md:pl-5 md:h-8"
         />
         <ul className="flex gap-2 h-full ">
           {items.map(({ id, path }) => (
@@ -32,6 +37,14 @@ export const Navbar: FC<Props> = ({ items }) => {
             </li>
           ))}
         </ul>
+        <img
+          src={
+            user.name.length ? "/img/user-masc.png" : "/img/user-masc-none.png"
+          }
+          alt={user.name.length ? user.name : "No logueado"}
+          className="px-2 ml-2 md:ml-3 h-6 pl-2 md:pl-3 md:h-8"
+          style={{ borderLeft: "solid 1px gray" }}
+        />
       </div>
     </div>
   );
