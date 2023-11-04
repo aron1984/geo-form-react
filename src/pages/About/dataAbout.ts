a7212e30a9be7bed4fb68168aa58d06565fed497
Alejandro Ronconi <ronconialejandro@gmail.com>
2023-11-04 14:10:33 -0300

    Agrego seccion about

---
+++ src/pages/About/dataAbout.ts
@@ -0,0 +1,35 @@
+const dataAbout = {
+  tools: [
+    { name: "Vite", link: "https://vitejs.dev/" },
+    {
+      name: "React",
+      link: "https://reactjs.org/",
+    },
+    {
+      name: "TypeScript",
+      link: "https://www.typescriptlang.org/",
+    },
+    {
+      name: "Tailwindcss",
+      link: "https://tailwindcss.com/",
+    },
+    {
+      name: "Leaflet",
+      link: "https://leafletjs.com/",
+    },
+    {
+      name: "Firebase",
+      link: "https://firebase.google.com/?hl=es",
+    },
+    {
+        name: "Zustand",
+        link: "https://zustand-demo.pmnd.rs/",
+    },
+    {
+      name: "Vercel",
+      link: "https://vercel.com/",
+    },
+  ],
+};
+
+export default dataAbout;
